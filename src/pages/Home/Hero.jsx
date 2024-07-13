250baba4c3d0b4457d0b7996dc5856db7e8013c7
Tuhinanshu Modgil <tuhinanshumodgil18@gmail.com>
2024-07-13 14:10:01 +0530

    added all data to data folder

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
     return (
       <div className="relative bg-white">
         <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-24 lg:px-8">
-          <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-32 xl:col-span-6">
+          <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-24 lg:pt-32 xl:col-span-6">
             <div className="mx-auto max-w-2xl lg:mx-0">
               
               <div className="hidden sm:mt-32 sm:flex lg:mt-16">
--- src/pages/Home/OurTeam.jsx
+++ src/pages/Home/OurTeam.jsx
@@ -1,32 +1,6 @@
-const people = [
-    {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-      },
-      {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-      },
-      {
-        name: 'Michael Foster',
-        role: 'Co-Founder / CTO',
-        imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-      },
-    // More people...
-  ]
+import { team } from "../../data/siteData"
   
-  export default function OurTeam() {
+export default function OurTeam() {
     return (
       <div className="bg-white pt-24 sm:pt-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -41,7 +15,7 @@ const people = [
             role="list"
             className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6"
           >
-            {people.map((person) => (
+            {team.map((person) => (
               <li key={person.name}>
                 <img className="mx-auto h-24 w-24 rounded-full" src={person.imageUrl} alt="" />
                 <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
