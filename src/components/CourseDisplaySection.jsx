25927bd0153f184a82be667f22bd68b1e4657fe5
Tuhinanshu Modgil <tuhinanshumodgil18@gmail.com>
2025-01-28 12:25:47 +0530

    fixed image handeling

--- src/components/CourseDisplaySection.jsx
+++ src/components/CourseDisplaySection.jsx
@@ -18,7 +18,7 @@ export default function CourseDisplaySection({ courses, sectionTitle="" }) {
                 <div className="relative w-full">
                   <img
                     // src={course.image}
-                    src={course.image.data}
+                    src={course?.image?.data?? "https://images.unsplash.com/photo-1501504905252-473c47e087f8?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                     alt={course.imageAlt}
                     className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
                   />
