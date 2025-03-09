c2149b2763ccfea33f25eb41cfefc9d2a345665d
Tuhinanshu Modgil <tuhinanshumodgil18@gmail.com>
2025-03-09 14:42:14 +0530

    removed old files

--- src/main.jsx
+++ src/main.jsx
@@ -11,14 +11,7 @@ import {
   Services,
   UpcomingCourses,
 } from "./pages/Home";
-import {
-  QIP_FAQs,
-  QIP_Features,
-  QIP_ImportantLinks,
-  QIP_PreviousCourses,
-  QIP_UpcomingCourses,
-  QIP_content,
-} from "./pages/QIP_Page/index.js";
+
 import CertificateForm from "./pages/Certificate/CertificateForm.jsx";
 import ContactUs from "./pages/ContactUs/ContactUs.jsx";
 import Announcements from "./pages/Home/Announcements.jsx";
@@ -26,8 +19,6 @@ import Registration from "./pages/Authentication/Registration.jsx";
 import CourseDetails from "./pages/CourseDetails/CourseDetails.jsx";
 import { AuthContextProvider } from "./context/authContext.jsx";
 // import CourseForm from "./components/CourseForm.jsx";
-import CEP_Page from "./pages/CEP_Page/CEP_Page.jsx";
-import Exec_Training_Page from "./pages/Exec_Training_Page/Exec_Training_Page.jsx";
 import Program_Page from "./pages/Program_Page/Program_Page.jsx";
 import EditProgramForm from "./components/EditProgramForm.jsx";
 import AddProgramForm from "./components/AddProgramForn.jsx";
@@ -56,27 +47,6 @@ const router = createBrowserRouter([
       {
         path: "/program/:id",
         element: <Program_Page />,
-      },
-      {
-        path: "/CEP",
-        element: <CEP_Page />,
-      },
-      {
-        path: "/QIP",
-        element: (
-          <div>
-            <QIP_content />
-            <QIP_Features />
-            {/* <QIP_UpcomingCourses /> */}
-            {/* <QIP_ImportantLinks /> */}
-            {/* <QIP_PreviousCourses /> */}
-            <QIP_FAQs />
-          </div>
-        ),
-      },
-      {
-        path: "/Exec_Training",
-        element: <Exec_Training_Page />,
       },
       {
         path: "/certificate",
